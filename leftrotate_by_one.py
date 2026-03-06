055660bfe9d229bd4c1d1533823d842e53c0d480
Chandana-5 <hchandana546@gmail.com>
2026-03-06 18:34:24 +0530

    Add files via upload

---
+++ leftrotate_by_one.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotateArrayByOne(self, nums):
+        n=len(nums)
+        temp=nums[0]
+        for i in range(1,n):
+            nums[i-1]=nums[i]
+        nums[n-1]=temp 
+        return n-1
