9987a4b71537d6327e1439492aa377186cd1849a
Chandana-5 <hchandana546@gmail.com>
2026-03-11 19:28:57 +0530

    Update recursive bubblesort.py

--- sorting/recursive bubblesort.py
+++ sorting/recursive bubblesort.py
@@ -1,13 +1,22 @@
+#algorithm
+#1.if size of array n1,stop
+#2.compare adjacent elements from index 0 to n-2
+#3.if nums[i]>nums[i+1],swap them
+#4. after one pass the largest elemet moves to the last position
+#5.recursively call bubblesort for the first n-1 elements 
+#6.repeat untill array is sorted
+#time complexity O(N^2)
+#space complexity O(N)
 class Solution:
     def bubbleSort(self, nums):
         n=len(nums)
         temp=[]
         if n==1:
             return nums
-        for i in range(n):
-            for j in range(n-i-1):
-                if nums[j]>nums[j+1]:
-                    temp=nums[j]
-                    nums[j]=nums[j+1]
-                    nums[j+1]=temp 
+        for i in range(n-1):
+                if nums[i]>nums[i+1]:
+                    temp=nums[i]
+                    nums[i]=nums[i+1]
+                    nums[i+1]=temp 
         return nums
+
