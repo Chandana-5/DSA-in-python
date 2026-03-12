41b7af57c5fc458c5d9fb8f1dd2cb9ddfded1a5f
Chandana-5 <hchandana546@gmail.com>
2026-03-12 22:25:36 +0530

    Refactor second largest element function with sorting
    Refactored second largest element function to use sorting for improved efficiency. Added comments explaining both brute force and optimal approaches.

--- arrays/secondlargest.py
+++ arrays/secondlargest.py
@@ -1,4 +1,26 @@
-class Solution:
+#Brute Force
+#algorithm
+#1.sort array in ascending order
+#2.second element from last index is second largest
+#time complexity O(N log N)
+#space complexity O(1)
+class solution:
+    def secondlargestelement(self,nums):
+        nums.sort()
+        return nums[-2]
+
+#optimal approach
+#algorithm
+#take variables largest and second largest
+#assign first element to largest
+#take -1 as second largest
+#if current element is larger than largest update values of second largest and largest
+#else if current element is larger than second largest update values of second largest
+#after sorting second largest wiil be stored in second largest
+#time complexity  O(N)
+#space complexity O(1)
+    
+    class Solution:
     def secondLargestElement(self, nums):
         n=len(nums)
         largest=nums[0]
@@ -13,3 +35,4 @@ class Solution:
 
         
         
+
