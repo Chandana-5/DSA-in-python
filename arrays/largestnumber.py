498a9cf6fd4388ea009df0a19e023020697d1b80
Chandana-5 <hchandana546@gmail.com>
2026-03-12 22:12:38 +0530

    Update largestnumber.py

--- arrays/largestnumber.py
+++ arrays/largestnumber.py
@@ -1,3 +1,25 @@
+#Brutte force
+#algorithm 
+#1.sort array in ascending order
+#2.print(size of array -1)th element
+#Time complexity O(N log N)
+#space complexity O(1)
+class solution:
+    def largestelement(self,nums):
+        nums.sort()
+        return nums[-1
+
+
+        
+#optimal opproach
+#algorithm
+#1.create variable largest
+#2.initialize with first element
+#3.use loop for rest element
+#4.compare current element with largest
+#5.if greater than update largest 
+#Time complexity O(N)
+#space complexity O(1)
 class Solution:
     def largestElement(self, nums):
         n=len(nums)
@@ -7,3 +29,4 @@ class Solution:
                 largest=nums[i]
         return largest
         
+
