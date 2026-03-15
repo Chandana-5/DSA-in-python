953456bdd0218a5eb3627ab7cec0bef5b9414edd
Chandana-5 <hchandana546@gmail.com>
2026-03-15 15:25:37 +0530

    Refactor removeDuplicates to use set for uniqueness
    Refactored removeDuplicates method to use a set for tracking unique elements, improving space complexity. Added comments explaining the algorithm and time complexity.

--- arrays/removing_duplicates.py
+++ arrays/removing_duplicates.py
@@ -1,4 +1,27 @@
+#brutte force
+#algorithm
+#1.declare a set and insert all the elements of array into set
+#2.number of unique elements i array is equal to size of the set
+#3.traverse the set and fill the first k indices with elements in set
+#time complexity - O(N)
+#space complexity - O(N)
 class Solution:
+    def removeDuplicates(self,nums):
+        seen=set()
+        index=0
+        for num in nums:
+            if num not in seen:
+                seen.add(num)
+                nums[index]=num
+                index+=1
+        return index
+#optimal solution
+#algorithm
+#1.check for unique element,run loop from 1 to n
+#2.in case of unique element store in new array ,nums[k] 
+#time complexity- O(N)
+#space complexity - O(1)
+    class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
         n=len(nums)
         k=1
